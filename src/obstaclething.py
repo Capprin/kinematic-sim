b275413a0e0bca1d1e81aa2750de1d6833079415
Capprin Bass <capprin@gmail.com>
2021-03-07 13:36:04 -0700

    Thing dependence (#3)
    * Added potential for Thing dependence in YAML construction
    
    * fixed typo

--- src/obstaclething.py
+++ src/obstaclething.py
@@ -13,7 +13,7 @@ class ObstacleThing(RandomThing):
     self.vote_exit = True
 
   @staticmethod
-  def from_dict(properties):
+  def from_dict(properties, previous_things=None):
     # abuse python's weak typing
     obs = RandomThing.from_dict(properties)
     obs.NAME = ObstacleThing.NAME
